5ba9c51e6406566d57055cff61ccfb6b66ea16d1
getch-geohum <getawork71@gmail.com>
2023-01-29 21:03:01 +0100

    Fake dataset site and model argument added

--- ae_train.sh
+++ ae_train.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 python3 ae_train.py\
+	--model=ssvae\
 	--exp=vae_vae\
-	--num_epochs=200\
+	--num_epochs=100\
 	--lr=1e-4\
 	--img_size=256\
 	--batch_size=16\
 	--batch_size_test=8\
-	--latent_img_size=8\
+	--latent_img_size=32\
 	--z_dim=256\
 	--lamda=0.9\
 	--nb_channels=4\
@@ -15,8 +16,8 @@ python3 ae_train.py\
 	--ndvi_treshold=0.1\
 	--intensity_treshold=120\
 	--func=NDVI\
-	--dst_dir=/home/getch/ssl/DATA/UAE/OUTS/Deghale_Apr_2017\
-        --data_dir=/home/getch/DATA/VAE/data/Deghale_Apr_2017\
+	--dst_dir=/home/getch/ssl/DATA/SSL_VAE/OUTS/Minawao_june_2016\
+        --data_dir=/home/getch/DATA/VAE/data/Minawao_june_2016\
 	--force_train\
 	#--equalize\
 
