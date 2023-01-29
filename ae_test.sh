c009d171950e412420b05d645c33304e98c153c3
getch-geohum <getawork71@gmail.com>
2023-01-29 21:00:30 +0100

    latent_img_size modified

--- ae_test.sh
+++ ae_test.sh
@@ -2,15 +2,16 @@
 
 python3 ae_test.py\
     --exp=vae_vae\
+    --model=ssvae\
     --num_epochs=100\
     --lr=1e-4\
     --img_size=256\
     --batch_size=16\
     --batch_size_test=8\
-    --latent_img_size=8\
+    --latent_img_size=32\
     --z_dim=256\
     --lamda=0.9\
     --nb_channels=4\
     --params_id=100\
-    --dst_dir=/home/getch/ssl/DATA/UAE/OUTS/Deghale_Apr_2017\
-    --data_dir=/home/getch/DATA/VAE/data/Deghale_Apr_2017\
+    --dst_dir=/home/getch/ssl/DATA/SSL_VAE/OUTS/Minawao_june_2016\
+    --data_dir=/home/getch/DATA/VAE/data/Minawao_june_2016\
