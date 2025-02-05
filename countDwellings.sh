951909106922edab083cf653cd6992752314e96c
Getachew Workineh Gella <82418348+getch-geohum@users.noreply.github.com>
2025-02-05 20:32:06 +0100

    Update countDwellings.sh

--- countDwellings.sh
+++ countDwellings.sh
@@ -1,6 +1,6 @@
 python countDwellings.py\
-	--root=D:/DATA/MVCamp_dis/outputs_ss_cvae_18_SSL_ELBO_COND\
-	--out_root=D:/DATA/OBJECT_METRICS\
+	--root=path2data\
+	--out_root=path2save\
 	--num_class=2\
 	--min_area=6\
 	--score=mads\
